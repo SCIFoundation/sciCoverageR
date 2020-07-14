b8bbe909d621fac6a1ceca0a838a743570f4d83d
Mario Gronert <mario.gronert@gmail.com>
2020-07-14 12:00:37 +0100

    Further work on the vignette.

--- R/equity_calc.R
+++ R/equity_calc.R
@@ -20,22 +20,24 @@ equity_calculation <- function(data, vectorOfQuintiles, equity_file){
 
   # Compare equity df from survey to values and options
   if (!identical(sort(names(data)), sort(unique(df$question)))){
-    stop("Col names of Equity Answer DF does not correspond with question names from scores dataframe")
+    stop("Col names of HH respondents DF does not correspond with question names from Equity scores DF")
   }
 
   # Ensure answer codes are the same
   for (q_name in names(data)){
     if (!all(unique(data[[q_name]])[!is.na(unique(data[[q_name]]))] %in%
              tibble::deframe(df[df$question == q_name, 2]))){
-      stop(sprintf("The answer codes for var %s in the Equity Answer df and the Scores df do not match", q_name))
+      stop(sprintf("The answer codes for var %s in the HH respondents DF and the Equity scores DF do not match", q_name))
     }
   }
 
   # Transpose equity questions data so that each individual is a column
   equity_transpose <- as.data.frame(t(data), stringsAsFactors = F)
   # Add the question category to the response to make responses unique for each individual
-  equity_transpose_codes <- purrr::map_dfc(as.list(seq(nrow(data))),
-                                           ~ paste(rownames(equity_transpose), equity_transpose[, .x], sep = " : "))
+  equity_transpose_codes <- purrr::map(as.list(seq(nrow(data))),
+                                       ~ paste(rownames(equity_transpose), equity_transpose[, .x], sep = " : "))
+  names(equity_transpose_codes) <- paste("hh", seq(length(equity_transpose_codes)), sep = "_")
+  equity_transpose_codes <- dplyr::bind_cols(equity_transpose_codes)
 
   # Scores for the individual are the sum of the values the responses
   score_vector <- purrr::map_dbl(equity_transpose_codes, ~ sum(df$score[match(.x, df$code)]))
