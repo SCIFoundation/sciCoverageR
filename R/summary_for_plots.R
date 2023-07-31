a78ed70c676f7711ffbf06d1f71077760b88a9e8
pancyp1 <p.poon@schisto.org.uk>
2023-07-31 16:38:58 +0100

    Added param to specify numeric outcomes in forplot function

--- R/summary_for_plots.R
+++ R/summary_for_plots.R
@@ -6,19 +6,23 @@
 #' @param ... Grouping variable(s)
 #' @param out An outcome variable
 #' @param dropna Should NAs be dropped from dataframe?
+#' @param number Is the type of outcome variable numeric?
 #'
 #' @return A summary tibble in long format for ggplot of n, total and percentage (n/total)
 #' where n is frequency of each level within the outcome variable and total is the sum of n, grouped by grouping variable(s)
 #' @export
 #'
 #' @examples When using function, need to specify out = otherwise function will assume it is a grouping variable
-forplot <- function(data, ..., out,dropna = c(FALSE,TRUE)){
+forplot <- function(data, ..., out,dropna = c(FALSE,TRUE), number= c(FALSE,TRUE)){
   dropna = assertive::use_first(dropna)
+  number= assertive::use_first(number)
   data %>%  group_by(...) %>% count({{out}}) %>%
     {if(dropna==TRUE) drop_na(.)  else .} %>%
     mutate(Total=sum(n, na.rm=T),
            perc=n/Total*100) %>%
-    arrange(desc(perc)) %>% ungroup() %>%
+    arrange(desc(perc)) %>%
+    {if(number==TRUE) {.} %>% arrange(desc(n)) else .} %>%
+    ungroup() %>%
 
     purrr::when(is.factor(pull(.,{{out}}))  ~ mutate(., {{out}} := forcats::fct_inorder({{out}})), ~ .)
 
